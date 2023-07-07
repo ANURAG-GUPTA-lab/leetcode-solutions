9dcdddd48151d7d42697adbcc94295b4a6796511
Anurag Gupta <75614852+ANURAG-GUPTA-lab@users.noreply.github.com>
2023-07-07 23:06:10 +0530

    Create Length of Last Word.java

---
+++ Length of Last Word.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+    int idx = s.length() - 1;
+
+    while (idx >= 0 && s.charAt(idx) == ' ') {
+
+      idx--;
+
+    }
+
+    int currIdx = idx;
+
+    while (idx >= 0 && s.charAt(idx) != ' ') {
+
+      idx--;
+
+    }
+
+    return currIdx - idx;
+    }
+}
